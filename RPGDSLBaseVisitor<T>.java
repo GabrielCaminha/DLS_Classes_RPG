b45b6cbf9119333faf9ccff13744540f579de84c
GabrielCaminha <89549264+GabrielCaminha@users.noreply.github.com>
2023-04-18 20:27:29 -0300

    Update RPGDSLBaseVisitor<T>.java

--- RPGDSLBaseVisitor<T>.java
+++ RPGDSLBaseVisitor<T>.java
@@ -1,30 +1,61 @@
 import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.TerminalNode;
+import org.antlr.v4.runtime.tree.ParseTree;
 
-public class RPGDSLBaseVisitor<T> extends RPGDSLParserBaseVisitor<T> {
+public class RPGDSLBaseVisitor extends RPGDSLParserBaseVisitor<Void> {
 
     @Override
-    public T visitClassDeclaration(@NotNull RPGDSLParser.ClassDeclarationContext ctx) {
-        return visitChildren(ctx);
+    public Void visitClassDeclaration(RPGDSLParser.ClassDeclarationContext ctx) {
+        return null;
     }
 
     @Override
-    public T visitAttributeDeclaration(@NotNull RPGDSLParser.AttributeDeclarationContext ctx) {
-        return visitChildren(ctx);
+    public Void visitAttributeDeclaration(RPGDSLParser.AttributeDeclarationContext ctx) {
+        return null;
     }
 
     @Override
-    public T visitItemDeclaration(@NotNull RPGDSLParser.ItemDeclarationContext ctx) {
-        return visitChildren(ctx);
+    public Void visitItemDeclaration(RPGDSLParser.ItemDeclarationContext ctx) {
+        return null;
     }
 
     @Override
-    public T visitActionDeclaration(@NotNull RPGDSLParser.ActionDeclarationContext ctx) {
-        return visitChildren(ctx);
+    public Void visitActionDeclaration(RPGDSLParser.ActionDeclarationContext ctx) {
+        return null;
     }
 
     @Override
-    public T visitTerminal(@NotNull TerminalNode node) {
+    public Void visitStatement(RPGDSLParser.StatementContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitRollStatement(RPGDSLParser.RollStatementContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitFunctionCall(RPGDSLParser.FunctionCallContext ctx) {
+        return null;
+    }
+
+    @Override
+    public Void visitTerminal(TerminalNode node) {
+        return null;
+    }
+
+    @Override
+    public Void visitChildren(@NotNull RuleNode node) {
+        int n = node.getChildCount();
+        for (int i = 0; i < n; i++) {
+            ParseTree c = node.getChild(i);
+            if (c instanceof RuleNode) {
+                ((RuleNode) c).accept(this);
+            }
+            else if (c instanceof TerminalNode) {
+                visitTerminal((TerminalNode) c);
+            }
+        }
         return null;
     }
 }
