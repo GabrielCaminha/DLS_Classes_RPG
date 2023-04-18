1f6ca69fc9578f04b14aa116d01ad337cf0151b3
GabrielCaminha <89549264+GabrielCaminha@users.noreply.github.com>
2023-04-18 19:51:15 -0300

    Update RPGDSLCodeGenerator.java

--- RPGDSLCodeGenerator.java
+++ RPGDSLCodeGenerator.java
@@ -1,51 +1,47 @@
-import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-public class RPGDSLCodeGenerator {
-    public static void main(String[] args) throws Exception {
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
-        RPGDSLLexer lexer = new RPGDSLLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        RPGDSLParser parser = new RPGDSLParser(tokens);
-        ParseTree tree = parser.input();
+public class CodeGenerator extends RPGDSLBaseVisitor<Void> {
 
-        RPGDSLVisitor<String> visitor = new RPGDSLBaseVisitor<String>() {
-            @Override
-            public String visitInput(RPGDSLParser.InputContext ctx) {
-                String output = "";
-
-                for (RPGDSLParser.ClassDeclarationContext classDecl : ctx.classDeclaration()) {
-                    String className = classDecl.className.getText();
-                    String classDescription = classDecl.description != null ? classDecl.description.getText() : "";
-
-                    output += String.format("public class %s {\n", className);
-                    output += String.format("\t// Description: %s\n", classDescription);
-                    output += "\tprivate int vida;\n";
-                    output += "\tprivate int mana;\n";
-
-                    output += "\n\t// Items\n";
-                    for (RPGDSLParser.ItemDeclarationContext itemDecl : classDecl.itemDeclaration()) {
-                        String itemName = itemDecl.itemName.getText();
-                        int itemDamage = Integer.parseInt(itemDecl.itemDamage.getText());
-                        output += String.format("\tprivate int %sDamage = %d;\n", itemName, itemDamage);
-                    }
-
-                    output += "\n\t// Actions\n";
-                    for (RPGDSLParser.ActionDeclarationContext actionDecl : classDecl.actionDeclaration()) {
-                        String actionName = actionDecl.actionName.getText();
-                        String actionDamage = actionDecl.damage != null ? actionDecl.damage.getText() : "0";
-                        output += String.format("\tpublic void %s() {\n", actionName);
-                        output += String.format("\t\tSystem.out.println(\"%s dealt %s damage!\");\n", actionName, actionDamage);
-                        output += "\t}\n";
-                    }
-
-                    output += "}\n\n";
-                }
-
-                return output;
-            }
-        };
-
-        System.out.print(visitor.visit(tree));
+    public void generate(ParseTree tree) {
+        visit(tree);
     }
+
+    @Override
+    public Void visitClassDeclaration(RPGDSLParser.ClassDeclarationContext ctx) {
+        String className = ctx.className.getText();
+        String classDescription = ctx.description != null ? ctx.description.getText() : "";
+
+        System.out.printf("public class %s {\n", className);
+        System.out.printf("\t// Description: %s\n", classDescription);
+        System.out.println("\tprivate int vida;");
+        System.out.println("\tprivate int mana;");
+
+        System.out.println("\n\t// Attributes");
+        for (RPGDSLParser.AttributeDeclarationContext attrDecl : ctx.attributeDeclaration()) {
+            String type = attrDecl.type.getText();
+            String name = attrDecl.attributeName.getText();
+            System.out.printf("\tprivate %s %s;\n", type, name);
+        }
+
+        System.out.println("\n\t// Items");
+        for (RPGDSLParser.ItemDeclarationContext itemDecl : ctx.itemDeclaration()) {
+            String itemName = itemDecl.itemName.getText();
+            int itemDamage = Integer.parseInt(itemDecl.damage.getText());
+            System.out.printf("\tprivate int %sDamage = %d;\n", itemName, itemDamage);
+        }
+
+        System.out.println("\n\t// Actions");
+        for (RPGDSLParser.ActionDeclarationContext actionDecl : ctx.actionDeclaration()) {
+            String actionName = actionDecl.actionName.getText();
+            String returnType = actionDecl.returnType != null ? actionDecl.returnType.getText() : "void";
+            String actionDamage = actionDecl.damage != null ? actionDecl.damage.getText() : "0";
+            System.out.printf("\tpublic %s %s() {\n", returnType, actionName);
+            System.out.printf("\t\tSystem.out.println(\"%s dealt %s damage!\");\n", actionName, actionDamage);
+            System.out.println("\t}");
+        }
+
+        System.out.println("}\n");
+        return null;
+    }
+
 }
