6a4af6823f37d3e468a15ebb1c3b908ef39a2085
GabrielCaminha <89549264+GabrielCaminha@users.noreply.github.com>
2023-04-18 20:17:24 -0300

    Update exemploImplementacao.java

--- exemploImplementacao.java
+++ exemploImplementacao.java
@@ -1,13 +1,17 @@
-class Warrior {
-    descricao "A fierce fighter";
+class Guerreiro {
+    descricao "Um guerreiro forte e corajoso";
     vida 100;
     mana 50;
-    int strength;
-    int agility;
-    item sword deals 10 damage;
-    item shield deals 5 damage;
-    action attack() {
-        : int;
-        20;
+    int forca 20;
+    int destreza 15;
+    int resistencia 18;
+
+    item espada deals 10 damage;
+    item escudo deals 5 damage;
+
+    action atacar() {
+        : int
+        10;
     }
 }
+
