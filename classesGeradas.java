530762176ac052fbbd1a899cbdbbea5b33457f91
GabrielCaminha <89549264+GabrielCaminha@users.noreply.github.com>
2023-04-18 20:20:31 -0300

    Update classesGeradas.java

--- classesGeradas.java
+++ classesGeradas.java
@@ -1,18 +1,25 @@
 public class Warrior {
-    private String descricao = "A fierce fighter";
-    private int vida;
-    private int mana;
+	private String = "Um guerreiro valente e forte";
+	private int vida = 100;
+	private int mana = 50;
 
-    // Attributes
-    private int strength;
-    private int agility;
+	// Attributes
+	private int forca = 10;
+	private int defesa = 5;
 
-    // Items
-    private int swordDamage = 10;
-    private int shieldDamage = 5;
+	// Items
+	private int espadaDamage = 15;
+	private int escudoDamage = 5;
 
-    // Actions
-    public int attack() {
-        System.out.println("attack dealt 20 damage!");
-    }
+	// Actions
+	public int atacar() {
+		int damage = espadaDamage + forca;
+		mana -= 10;
+		return damage;
+	}
+
+	public void defender() {
+		defesa += escudoDamage;
+		mana -= 5;
+	}
 }
