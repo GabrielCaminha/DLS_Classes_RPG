741517b0145d3911718c3b8de9817127b5fe782c
GabrielCaminha <89549264+GabrielCaminha@users.noreply.github.com>
2023-04-26 18:58:25 -0300

    Update classesGeradas.java

--- classesGeradas.java
+++ classesGeradas.java
@@ -1,25 +1,66 @@
-public class Warrior {
-	private String = "Um guerreiro valente e forte";
-	private int vida = 100;
-	private int mana = 50;
+public class Guerreiro {
+    private String descricao = "Classe focada em combate corpo-a-corpo.";
+    private int vida = 150;
+    private int mana = 50;
 
-	// Attributes
-	private int forca = 10;
-	private int defesa = 5;
+    // Attributes
+    private int forca = 10;
+    private int destreza = 8;
+    private int constituição = 12;
 
-	// Items
-	private int espadaDamage = 15;
-	private int escudoDamage = 5;
+    // Items
+    private int espadaDamage = 15;
+    private int poçãoHealing = 25;
 
-	// Actions
-	public int atacar() {
-		int damage = espadaDamage + forca;
-		mana -= 10;
-		return damage;
-	}
+    // Actions
+    public void atacar() {
+        destreza += 2;
+        forca += 5;
+    }
 
-	public void defender() {
-		defesa += escudoDamage;
-		mana -= 5;
-	}
+    public void beberPoção() {
+        vida += poçãoHealing;
+    }
+
+    // Getters e Setters para os itens
+    public int getEspadaDamage() {
+        return espadaDamage;
+    }
+
+    public void setEspadaDamage(int damage) {
+        espadaDamage = damage;
+    }
+
+    public int getPoçãoHealing() {
+        return poçãoHealing;
+    }
+
+    public void setPoçãoHealing(int healing) {
+        poçãoHealing = healing;
+    }
+
+    // Getters e Setters para os atributos
+    public int getForca() {
+        return forca;
+    }
+
+    public void setForca(int amount) {
+        forca = amount;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(int amount) {
+        destreza = amount;
+    }
+
+    public int getConstituição() {
+        return constituição;
+    }
+
+    public void setConstituição(int amount) {
+        constituição = amount;
+    }
 }
